bbc909c434c5bcf64f5e7f3b60c893935e5f9a79
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2020-12-27 21:02:40 -0500

    Eleven.java

--- src/review/Eleven.java
+++ src/review/Eleven.java
@@ -3,10 +3,14 @@ package review;
 import java.io.File;
 
 public class Eleven {
+    public static void main(String[] args) {
+        deleteTree(new File("storage"));
+    }
     public static void deleteTree(File file) {
         if(!file.isFile()) // f1
             for(File entry: file.listFiles()) // f2
                 deleteTree(entry);
-        else file.delete();
+        else
+            file.delete();
     }
 }
